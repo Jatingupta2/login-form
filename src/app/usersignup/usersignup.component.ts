ada15f5810906cec1ef04d76c42986f170d1259d
Jatingupta2 <jatingupta9213037402@gmail.com>
2023-10-27 13:19:44 +0530

    d

--- src/app/usersignup/usersignup.component.ts
+++ src/app/usersignup/usersignup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl , FormGroup, Validators } from '@angular/forms';
+import { FormControl , FormGroup, Validators,AbstractControl,ValidationErrors } from '@angular/forms';
 
 
 @Component({
@@ -9,16 +9,15 @@ import { FormControl , FormGroup, Validators } from '@angular/forms';
 })
 export class UsersignupComponent {
   signupform= new FormGroup({
-    username: new FormControl('',[Validators.required]),
+    username: new FormControl('',[Validators.required,Validators.maxLength(6)]),
     email: new FormControl('',[Validators.required,Validators.email]),
     password: new FormControl('',[Validators.required]),
     confirmpassword: new FormControl('',[Validators.required]),
-
-
-     
-  })
+}, {
+  validators: this.passwordMatchValidator,
+})
   signupuser(){
-    console.log(this.signupform.value)
+   console.log('hello');
   }
   get username(){
     return this.signupform.get( 'username')
@@ -47,6 +46,21 @@ confirmviewpass(){
   this.confirmvisible =!this.confirmvisible;
   this.confirminputtype=!this.confirminputtype;
 
+}
+ passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmpassword');
+
+  if (!password || !confirmPassword) {
+    // If either field is null, do not perform validation
+    return null;
+  }
+
+  if (password.value !== confirmPassword.value) {
+    return { passwordMismatch: true };
+  } else {
+    return null;
+  }
 }
 
 
